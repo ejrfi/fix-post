0c30b80bdf5a2fc03d4bc4279a1a15c753c91469
ejrfi <113613035+ejrfi@users.noreply.github.com>
2026-04-08 22:06:19 +0900

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -13,7 +13,7 @@ export function serveStatic(app: Express) {
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
-  app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
     if (req.path.startsWith("/api")) {
       return res.status(404).json({ message: "API route not found" });
     }
